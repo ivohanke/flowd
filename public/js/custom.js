0e03c872d98a6ddced3b3662c801394a6aa17739
Ivo Hanke <mail@ivohanke.de>
2014-02-02 01:20:33 +0100

    bugfixes

--- public/js/custom.js
+++ public/js/custom.js
@@ -29,22 +29,22 @@ App = {
     $(document).on('shown.bs.modal', function (e) {
       var html;
       if ($(e.relatedTarget).data('action') == 'edit') {
+        $('.modal-title', e.target).html('Edit note');
+        $('.modal-footer button', e.target).html('Save changes');
         $.ajax({
           url: '/note/' + $(e.relatedTarget).data('guid'),
           data: {
             format: 'json'
           },
           success: function(result) {
-            $('.modal-title', e.target).html(result.title);
+            $('.modal-note-title', e.target).html(result.title);
             $('.modal-note-tags', e.target).html(result.tags);
-            $('.modal-note-content', e.target).html(result.content);
-            $('.modal-note-content', e.target).tinymce({
+            $('#modal-note-content', e.target).html(result.content).tinymce({
               script_url : '/tinymce/tinymce.min.js',
               skin: 'light',
               menubar: false,
               statusbar: false
             });
-            $('.modal-footer button', e.target).html('Save changes');
           },
           error: function() {
 
@@ -52,13 +52,33 @@ App = {
         });
       } else if ($(e.relatedTarget).data('action') == 'send') {
         $('.modal-title', e.target).html('Send note');
-        html = '<p>Send this note by mail</p>';
-        html += '<div class="form-group"><input type="text" class="form-control" placeholder="Enter email"></div>';
-        $('.modal-note-content', e.target).html(html);
+        html = '<div class="modal-send"><p>Send this note by mail</p>';
+        html += '<div class="form-group"><input type="text" class="form-control" placeholder="Enter email"></div></div>';
+        $('.modal-note-action', e.target).html(html);
         $('.modal-footer button', e.target).html('Send');
+        $.ajax({
+          url: '/note/' + $(e.relatedTarget).data('guid'),
+          data: {
+            format: 'json'
+          },
+          success: function(result) {
+            $('.modal-note-title', e.target).html(result.title);
+            $('.modal-note-tags', e.target).html(result.tags);
+            $('#modal-note-content', e.target).html(result.content).tinymce({
+              script_url : '/tinymce/tinymce.min.js',
+              skin: 'light',
+              menubar: false,
+              statusbar: false
+            });
+            $('.modal-footer button', e.target).html('Send');
+          },
+          error: function() {
+
+          }
+        });
       } else if ($(e.relatedTarget).data('action') == 'create') {
         $('.modal-title', e.target).html('Create new note');
-        $('.modal-note-content', e.target).tinymce({
+        $('#modal-note-content', e.target).tinymce({
           script_url : '/tinymce/tinymce.min.js',
           skin: 'light',
           menubar: false,
@@ -68,12 +88,13 @@ App = {
       }
     });
 
-    $(document).on('hidden.bs.modal', function (e) {
-      $('.modal-note-content', e.target).tinymce().remove();
-      $('.modal-title', e.target).html('Loading');
-      $('.modal-note-tags', e.target).empty();
-      $('.modal-note-content', e.target).empty();
-    });
+    // $(document).on('hidden.bs.modal', function (e) {
+    //   $('.modal-note-content').empty();
+    //   $('#modal-note-content').tinymce().destroy();
+    //   $('.modal-note-title').html('Loading...');
+    //   $('.modal-note-action').empty();
+    //   $('.modal-note-tags').empty();
+    // });
   },
 
   handleDragDrop: function() {
